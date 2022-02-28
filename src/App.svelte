50c45ebd573c84a71de105a42e2f717ce2d59eee
Tom Rothamel <tom@rothamel.us>
2022-02-27 23:36:38 -0500

    Implement.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,317 @@
-<script>
-	export let name;
-</script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+  <!-- Add your site or application content here -->
+<div class="container">
+
+<div class="row">
+	<div class="col-sm-12">
+	<img src="images/2022.png" style="width: 100%; margin-bottom: 1em;" class="text-center"
+		alt="NaNoRenO: Visual Novel Game Jam"
+	>
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-sm-12">
+	<p>NaNoRenO is a online game jam in which developers are challenged to make a visual
+		novel from scratch. It runs from the start of March 1st to the end of March 31st, and has
+		been run every year since 2005. NaNoRenO has no judging or prizes - the only rewards are
+		those that come from the challenge and camaraderie with others.</p>
+
+	<p>
+		NaNoRenO has always been organized by the English-language visual novel development community,
+		especially the members of the <a href="http://lemmasoft.renai.us/forums/index.php">Lemma Soft Forums</a>. This
+		site serves as a hub to help find their many efforts.
+	</p>
+
+	<p>
+		NaNoRenO { year }
+
+		{#if start > now}
+		starts in
+		{ startDays } { startDaysWord }, {startHours}:{startMinutes}:{startSeconds}
+		and
+		{/if}
+
+		ends in
+		<b>
+		{ endDays } { endDaysWord }, {endHours}:{endMinutes}:{endSeconds}
+
+		</b>
+	</p>
+
+
+	</div>
+</div>
+
+
+<h3 class="text-center">Current Year</h3>
+
+
+<div class="text-center">
+<a href="http://lemmasoft.renai.us/forums/viewforum.php?f=50" class="btn btn-link btn-lg">NaNoRenO
+	Works in Progress Forum</a>
+</div>
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2022</h4>
+
+	<div class="text-center">
+		<img src="images/2022.png" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+	<div class="col-md-6 text-center">
+		<a href="https://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=64006" class="btn btn-link btn-lg">Recruitment Thread</a>
+	</div>
+	<div class="col-md-6 text-center">
+		<a href="http://itch.io/jam/nanoreno-2022" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+
+
+<h3 class="text-center">Previous Years</h3>
+
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2021</h4>
+
+	<div class="text-center">
+		<img src="images/2021.png" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+	<div class="col-md-6 text-center">
+		<a href="https://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=61356" class="btn btn-link btn-lg">Recruitment Thread</a>
+	</div>
+	<div class="col-md-6 text-center">
+		<a href="http://itch.io/jam/nanoreno-2021" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2020</h4>
+
+	<div class="text-center">
+		<img src="images/2020.png" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+		<div class="col-md-4 text-center">
+			<a href="https://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=58073" class="btn btn-link btn-lg">Announcement Thread</a>
+		</div>
+		<div class="col-md-4 text-center">
+			<a href="https://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=58106" class="btn btn-link btn-lg">Recruitment Thread</a>
+		</div>
+			<div class="col-md-4 text-center">
+		<a href="http://itch.io/jam/nanoreno-2020" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2019</h4>
+
+	<div class="text-center">
+		<img src="images/2019.png" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+	<div class="col-md-6 text-center">
+		<a href="https://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=53763" class="btn btn-link btn-lg">Recruitment Thread</a>
+	</div>
+	<div class="col-md-6 text-center">
+		<a href="http://itch.io/jam/nanoreno-2019" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2018</h4>
+
+	<div class="text-center">
+		<img src="images/2018.png" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+	<div class="col-md-6 text-center">
+		<a href="https://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=48005" class="btn btn-link btn-lg">Recruitment Thread</a>
+	</div>
+	<div class="col-md-6 text-center">
+		<a href="http://itch.io/jam/nanoreno-2018" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2017</h4>
+
+	<div class="text-center">
+		<img src="images/2017.jpg" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+	<div class="col-md-6 text-center">
+		<a href="https://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=42386" class="btn btn-link btn-lg">Recruitment Thread</a>
+	</div>
+	<div class="col-md-6 text-center">
+		<a href="http://itch.io/jam/nanoreno-2017" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2016</h4>
+
+	<div class="text-center">
+		<img src="images/2016.png" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+	<div class="col-md-4 text-center">
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=13&t=37034" class="btn btn-link btn-lg">Main/Rules Thread</a>
+	</div>
+	<div class="col-md-4 text-center">
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=37057" class="btn btn-link btn-lg">Recruitment Thread</a>
+	</div>
+	<div class="col-md-4 text-center">
+		<a href="http://itch.io/jam/nanoreno-2016" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=50&t=28161" class="btn btn-link btn-lg">NaNoRenO
+	Games List</a>
+
+<div class="card card-body bg-light nn-year">
+	<h4 class="text-center">2015</h4>
+
+	<div class="text-center">
+		<img src="images/2015.jpg" class="rounded" style="max-width: 400px">
+	</div>
+
+	<div class="row">
+	<div class="col-md-4 text-center">
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=13&t=30252" class="btn btn-link btn-lg">Main/Rules
+		Thread</a>
+	</div>
+	<div class="col-md-4 text-center">
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=59&t=30327" class="btn btn-link btn-lg">Recruitment
+		Thread</a>
+	</div>
+	<div class="col-md-4 text-center">
+		<a href="http://itch.io/jam/nanoreno-2015" class="btn btn-link btn-lg">Itch.io Jam Page</a>
+	</div>
+	</div>
+
+</div>
+
+<div class="row">
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2014</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=26192" class="btn btn-link btn-lg">Post-Mortem Thread</a>
+	</div>
+	</div>
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2013</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=19232" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2012</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=13992" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+
+</div>
+
+<div class="row">
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2011</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=9380" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2010</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=6636" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2009</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=4709" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+
+</div>
+
+<div class="row">
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2008</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=3011" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2007</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=1750" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+	<div class="col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2006</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=943" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+
+</div>
+
+<div class="row">
+
+	<div class="col-md-offset-4 col-md-4 text-center">
+	<div class="card card-body bg-light nn-year">
+		<h4 class="text-center">2005</h4>
+		<a href="http://lemmasoft.renai.us/forums/viewtopic.php?f=4&t=428" class="btn btn-link btn-lg">Main Thread</a>
+	</div>
+	</div>
+
+
+</div>
+
+</div>
+
+<hr>
+
+<p class="text-center">If you find issues with this website, <a href="https://www.renpy.org/email">email me</a>.
+</p>
 
 <style>
 	main {
@@ -27,4 +333,109 @@
 			max-width: none;
 		}
 	}
+
+	.nn-year {
+		margin-bottom: 1em;
+		padding-bottom: 0px;
+	}
+
+	IMG {
+		padding-bottom: .5em;
+	}
+
 </style>
+
+
+<script>
+
+	let year;
+
+	let now;
+	let start;
+	let end;
+
+	let startDays;
+	let startDaysWord;
+	let startHours;
+	let startMinutes;
+	let startSeconds;
+
+	let endDays;
+	let endDaysWord;
+	let endHours;
+	let endMinutes;
+	let endSeconds;
+
+	function update() {
+		now = new Date();
+
+		year = now.getFullYear();
+		var month = now.getMonth();
+
+		// Zero-centered months mean 3 is April.
+		if (month >= 3) {
+			year += 1;
+		}
+
+		start = new Date(year, 2, 1, 0, 0, 0);
+		end = new Date(year, 3, 1, 0, 0, 0);
+
+		start = +start;
+		end = +end;
+		now = +now;
+
+		console.log(now < start);
+
+		function format(n, divide, modulo, pad) {
+				n = n / divide;
+				n = n % modulo;
+				n = Math.floor(n);
+				if (pad) {
+						if (n<10) {
+								n = '0' + n;
+						}
+				}
+
+				return n;
+		}
+
+		now = now
+		start = start;
+		end = end;
+
+		startDays = format(start - now, 86400000, 366, false);
+		startHours = format(start - now, 3600000, 24, true);
+		startMinutes = format(start - now, 60000, 60, true);
+		startSeconds = format(start - now, 1000, 60, true);
+
+		if (startDays == 1) {
+			startDaysWord = "day";
+		} else {
+			startDaysWord = "days";
+		}
+
+		endDays = format(end - now, 86400000, 366, false);
+		endHours = format(end - now, 3600000, 24, true);
+		endMinutes = format(end - now, 60000, 60, true);
+		endSeconds = format(end - now, 1000, 60, true);
+
+		if (endDays == 1) {
+			endDaysWord = "day";
+		} else {
+			endDaysWord = "days";
+		}
+
+	}
+
+	update();
+
+	setInterval(update, 100);
+
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-1855234-12', 'auto');
+	ga('send', 'pageview');
+</script>
